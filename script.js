1666934df19e2a4049a41c4fef47c8489d102021
Jesse <jessecdenier@gmail.com>
2024-01-17 21:32:27 -0600

    Set up basic HTML and CSS.

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add your JavaScript logic here for handling the quiz and timer
+// You can use JavaScript to dynamically generate questions and options
+// and handle the timer functionality.
+
+// Example function for submitting the quiz
+function submitQuiz() {
+  // Add your logic for quiz submission
+}
